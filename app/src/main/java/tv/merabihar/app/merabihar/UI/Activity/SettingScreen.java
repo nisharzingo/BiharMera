7f5d1a92029474ec10dc81e803e577b3bebfcc5a
nisharzingo <nishar@zingohotels.com>
2018-11-05 16:40:17 +0530

    settings

---
+++ app/src/main/java/tv/merabihar/app/merabihar/UI/Activity/SettingScreen.java
@@ -0,0 +1,15 @@
+package tv.merabihar.app.merabihar.UI.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import tv.merabihar.app.merabihar.R;
+
+public class SettingScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setting_screen);
+    }
+}
